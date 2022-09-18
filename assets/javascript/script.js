786c1e86f9ae38af53a78b33b4dea6f010b1c471
reneenh <reneehamlinteaches@gmail.com>
2022-09-18 14:54:55 -0700

    some CSS fixes, changed temp. to Fahrenheit, fixed temp. display code errors.

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,34 +1,43 @@
 var weatherAPIkey = "599aa725fe51b1470ed131616e7d07c9";
-var searchButton = document.querySelector(".btn")
-var cityInput = document.getElementById("city")
-
-
+var searchButton = document.querySelector(".btn");
+var cityInput = document.getElementById("city");
 
 function getCoordinates(e) {
-    e.preventDefault()
-   var city = cityInput.value
-   console.log(city)
-    var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + weatherAPIkey 
-    fetch(queryURL).then(response => {
-    return response.json()
-}).then(data=>{
+  e.preventDefault();
+  var city = cityInput.value;
+  console.log(city);
 
-    // getWeather(data.coord.lat, data.coord.lon)
-    displayWeather(data)
-})
+  if (!city || city === "") return;
+
+  var queryURL =
+    "https://api.openweathermap.org/data/2.5/weather?q=" +
+    city +
+    "&appid=" +
+    weatherAPIkey;
+  fetch(queryURL)
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      console.log(`coordinates: `, data);
+      getWeather(data.coord.lat, data.coord.lon);
+    });
 }
 
 function getWeather(lat, lon) {
-    var queryURL2 = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&appid=" + weatherAPIkey
-fetch(queryURL2).then(response => {
-    return response.json()
-}).then(data=>{
-    console.log(data)
+  console.log("get weather function");
+  var queryURL2 = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=imperial&appid=${weatherAPIkey}`;
 
-})
+  fetch(queryURL2)
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      console.log("weather data, ", data);
+      displayWeather(data);
+    });
 }
 function displayWeather(data) {
-    console.log(data)
-    document.getElementById("temp").textContent=data.main.temp
+  document.getElementById("temp").textContent = data.main.temp;
 }
-searchButton.addEventListener("click", getCoordinates)
+searchButton.addEventListener("click", getCoordinates);
